21f024e08a11a6d90d2698b46cd92b65d0c269ab
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-17 18:38:38 +1000

    Using webview package to load website as a mobile app.

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Welcome from "./components/Welcome";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   const names = ["Islam", "Yasmin", "Farida", "Zain"];
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-      {names.map((item) => (
-        <Welcome name={item} />
-      ))}
-    </View>
+    <WebView
+      source={{ uri: "https://home-style.netlify.app/" }}
+      style={{ flex: 1 }}
+    />
+    // <View style={styles.container}>
+    //   <Text>Hello World</Text>
+    //   <StatusBar style="auto" />
+    //   {names.map((item) => (
+    //     <Welcome name={item} key={item} />
+    //   ))}
+    // </View>
   );
 }
 
